2ff896979edf62e11030b8590430961402b069ef
imranhasan2 <imranhasan030303@gmail.com>
2024-02-19 00:53:02 +0600

    finished design part

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,46 @@ module.exports = {
   };
 
 
-  
+  // chatgpt
+//   section class="bg-white p-8">
+//   <div class="flex justify-around">
+//     <div class="flex gap-10">
+//       <div class="flex gap-3">
+//         <img class="max-w-12 max-h-11" src="images/bus-logo.png" alt="">
+//         <div>
+//           <h2 class="text-3xl font-bold">Greenline Paribahan</h2>
+//           <p>Coach-009-WEB! AC_Business</p>
+//         </div>
+//       </div>
+//       <div>
+//         <button class="btn bg-[#1DD100]">
+//           <img src="images/seat-green-filled.png" alt="">
+//           <span>40</span> Seats left
+//         </button>
+//       </div>
+//     </div>
+//     <!-- form section -->
+//     <div class="bg-[#F7F8F8] p-4">
+//       <div class="flex justify-between mb-4">
+//         <h1>Route</h1>
+//         <h1>Dhaka - Sylhet</h1>
+//       </div>
+//       <hr class="mb-4">
+//       <div class="flex justify-between mb-4">
+//         <h1>Departure Time</h1>
+//         <p>9:00 PM</p>
+//       </div>
+//       <hr class="mb-4">
+//       <div class="flex justify-between shadow-md">
+//         <button class="bg-[#F7F8F8]">Boarding Point - Laxmipur</button>
+//         <button class="bg-[#F7F8F8]">Dropping Point - Bogura</button>
+//         <button class="bg-[#F7F8F8]">Est. Time - 11 Hour</button>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="flex flex-col space-y-4 justify-center items-center mt-8">
+//     <img src="images/fare.png" alt="">
+//     <h1><span>550</span> Taka</h1>
+//     <p>Per Seat</p>
+//   </div>
+// </section>
